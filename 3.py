39299919a8402b78e534a2b794dedc1554e5a1b9
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-11-09 13:16:35 +0330

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+import jdatetime
+
+# now = str(jdatetime.datetime.now())
+# print(now)
+# print(now.split())
+
+christmas = jdatetime.date.fromgregorian(day=25, month=12, year=2023)
+print(christmas)
